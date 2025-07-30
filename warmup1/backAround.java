c26e73966d8b05806a43fd25e51a5a9a6eaf152f
Rudaina <rudainaalb26@gmail.com>
2025-07-30 05:27:20 +0400

    Add solution for warmup1-backAround

---
+++ warmup1/backAround.java
@@ -0,0 +1,6 @@
+public class BackAround {
+    public String backAround(String str) {
+        String lastChar = str.substring(str.length() - 1);
+        return lastChar + str + lastChar;
+    }
+}
