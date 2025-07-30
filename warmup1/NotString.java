16970e50b4268465388b7ed7476e916b72060a2f
Rudaina <rudainaalb26@gmail.com>
2025-07-30 04:40:59 +0400

    Add solution for warmup1-notString

---
+++ warmup1/NotString.java
@@ -0,0 +1,9 @@
+public class NotString {
+    public String notString(String str) {
+        if (str.startsWith("not")) {
+            return str;
+        }
+        return "not " + str;
+    }
+}
+
