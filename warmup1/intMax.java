548d8db331cb1ee019493189c135fa297110364d
Rudaina <rudainaalb26@gmail.com>
2025-07-30 19:48:50 +0400

    Add solution for warmup1-intMax

---
+++ warmup1/intMax.java
@@ -0,0 +1,12 @@
+public class IntMax {
+    public int intMax(int a, int b, int c) {
+        int max = a;
+        if (b > max) {
+            max = b;
+        }
+        if (c > max) {
+            max = c;
+        }
+        return max;
+    }
+}
