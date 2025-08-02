240d0a50cd39c01b5392a75eb1e9e4cac81d9832
Rudaina <rudainaalb26@gmail.com>
2025-08-03 03:24:11 +0400

    Add solution for string1-makeOutWord

---
+++ string1/MakeOutWord.java
@@ -0,0 +1,7 @@
+public class MakeOutWord {
+    public String makeOutWord(String out, String word) {
+        String start = out.substring(0, 2);
+        String end = out.substring(2);
+        return start + word + end;
+    }
+}
